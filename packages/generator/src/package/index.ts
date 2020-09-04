4b9758bb0253fa7c9d2d05cfdff9aabeeb6bce6e
John Wright <jrite@protonmail.com>
2020-09-04 12:00:18 +1000

    feat: initial import
    affects: @johngeorgewright/generator, @johngeorgewright/ts-mono-repo-package
    
    Using a previous package

---
+++ packages/generator/src/package/index.ts
@@ -0,0 +1,70 @@
+import Generator from 'yeoman-generator'
+import { paramCase } from 'change-case'
+import { validateGenerationFromRoot } from '../validation'
+import * as path from 'path'
+
+export = class PluginGenerator extends Generator {
+  private answers: { description?: string; name?: string }
+
+  constructor(args: string | string[], opts: Record<string, unknown>) {
+    super(args, opts)
+    this.answers = {}
+  }
+
+  initializing() {
+    validateGenerationFromRoot(this)
+  }
+
+  async prompting() {
+    this.answers = await this.prompt([
+      {
+        message:
+          "What is the packages's name? (Minus the @johngeorgewright namespace)",
+        name: 'name',
+        type: 'input',
+        validate: (x) => !!x || 'You must supply a name',
+      },
+      {
+        message: "What's this package about?",
+        name: 'description',
+        type: 'input',
+      },
+    ])
+  }
+
+  configuring() {
+    this.destinationRoot(`packages/${paramCase(this.answers.name!)}`)
+    this.sourceRoot(path.resolve(__dirname, '..', '..', 'templates'))
+  }
+
+  writing() {
+    const context = {
+      description: this.answers.description || '',
+      name: paramCase(this.answers.name!),
+    }
+
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
+      context
+    )
+
+    this.fs.copy(
+      this.templatePath('tsconfig.json'),
+      this.destinationPath('tsconfig.json')
+    )
+
+    this.fs.copy(
+      this.templatePath('jest.config.json'),
+      this.destinationPath('jest.config.json')
+    )
+
+    this.fs.copyTpl(
+      this.templatePath('package-src/index.ts.template'),
+      this.destinationPath('src/index.ts'),
+      context
+    )
+  }
+
+  install() {}
+}
