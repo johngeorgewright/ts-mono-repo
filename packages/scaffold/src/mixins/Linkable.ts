8e97424d9008307d1382384837a84d0e03a80df3
John Wright <jrite@protonmail.com>
2025-04-18 16:53:37 +0100

    refactor(scaffold): abstract linkable options

---
+++ packages/scaffold/src/mixins/Linkable.ts
@@ -0,0 +1,28 @@
+import { Option } from 'clipanion'
+import { packagePath } from '../workspace.js'
+
+type BaseConstructor = abstract new (...args: any[]) => {}
+
+export function Linkable<TBase extends BaseConstructor>(Base: TBase) {
+  abstract class Linkable extends Base {
+    readonly src = Option.String({
+      name: 'source',
+      required: true,
+    })
+
+    readonly dest = Option.String({
+      name: 'destination',
+      required: true,
+    })
+
+    get srcPackagePath() {
+      return packagePath(this.src)
+    }
+
+    get destPackagePath() {
+      return packagePath(this.dest)
+    }
+  }
+
+  return Linkable
+}
